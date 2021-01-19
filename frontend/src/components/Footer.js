192a823c276ccc78be68d6852545c06b0a51554d
Raj-Ailani <ailaniraj1@gmail.com>
2021-01-20 00:37:45 +0530

    Header And Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,8 +8,39 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 const Footer = (props) =>{
     return(
-            <div>
-                Footer
+            <div id='footer'>
+               <div className="container" >
+            <div className="row justify-content-center" id='footer-cont'>             
+                <div className="col-4 offset-1 col-sm-2">
+                    <h5>Links</h5>
+                    <ul className="list-unstyled">
+                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/about">AboutUs</Link></li>
+                       <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </div>
+                <div className="col-7 col-sm-5">
+                    <h5>Our Address</h5>
+                    <address>Shop No-3,Ground Floor,Satyam Complex
+		              <br />Opp. Balaji Garden Resturant,Near Prenatirth Road ,Jodhpur Gam
+		             <br />Satelite,Ahmedabad-15
+		             
+                      </address>
+                      <br/>
+		              <i className="fa fa-phone fa-lg"></i>:+91 8866091788<br />
+		              
+		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+                         Mail</a>
+                   
+                </div>
+          
+            </div>
+            <div className="row justify-content-center">             
+                <div className="col-auto">
+                <br/>  <p>© Copyright 2020 VEGGIE</p>
+                </div>
+            </div>
+        </div>
             </div>
     )
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,42 @@
 import React, { Component ,useS, useState} from 'react'
 import {useDispatch,useSelector} from 'react-redux'
-import {Nav,Navbar,NavbarBrand,NavbarToggler,Collapse, Container, NavItem} from 'reactstrap';
+import {Nav,Navbar,NavbarBrand,NavbarToggler,Collapse, Container,Col, NavItem, Button} from 'reactstrap';
 import {Link, NavLink} from 'react-router-dom';
 import { NavDropdown } from 'react-bootstrap';
+
 
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = (props) =>{
+    const [isNavOpen,toggleNav] = useState(false)
+      
     return(
         <div id='header'>
-        <Navbar dark id="navbar" expand="md">
-      
-            <NavbarBrand>
-                <Container id="logo-con" className="mr-auto"> 
-                    <NavLink to="/home">
-                        <img src="/assets/logo.png" alt="logo" className="logo"></img>
-                    </NavLink>
-                </Container>
-            </NavbarBrand>
+            <Navbar id='navbar' expand='md'>
+            <NavbarToggler onClick={()=>toggleNav(!isNavOpen)}><i className="fa fa-bars" id='bars'></i></NavbarToggler>
+           
+                <Container id='logo-cont' className='mr-auto'>
+                            <NavLink to="/home">
+                                <img src="/assets/logo.png" alt="logo" className="logo"></img>
+                            </NavLink>
+                 </Container>
 
-       
-        </Navbar>
-    </div>
+
+                 <Collapse isOpen={isNavOpen} navbar>
+                     <Nav id='nav-items' className='ml-auto'><NavItem >
+                        <NavLink className="nav-item" to="/cart"> Cart &ensp;<i className="fa fa-shopping-basket" /></NavLink>
+                        </NavItem>
+                        <NavItem >
+                        <NavLink className="nav-item" to="/cart"> Sign In &ensp;<i className="fa fa-user" /></NavLink>
+                        </NavItem>
+                        </Nav>
+               </Collapse>
+
+                
+                 
+            
+            </Navbar>    
+         </div>
     )
 }
 
