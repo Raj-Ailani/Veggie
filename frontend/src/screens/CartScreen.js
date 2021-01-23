c393508ddd5fc533bb2727ac911859b65d490d84
Raj-Ailani <ailaniraj1@gmail.com>
2021-01-23 14:18:06 +0530

    changes in cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -38,16 +38,16 @@ const CartScreen = ({match,location,history}) => {
                             cartItems.map(item =>(
                                 <ListGroupItem key={item.product} id='cart-items'>
                                     <Row>
-                                        <Col md={2}>
+                                        <Col md={2} xs={3} sm={3}>
                                             <Image src={item.image} alt={item.name} fluid rounded/>
                                         </Col>
-                                        <Col md={3} >
+                                        <Col md={3} xs={2} sm={2}>
                                              <Link to={`/product/${item.product}`}>{item.name}</Link>
                                         </Col>
-                                        <Col md={2} >
+                                        <Col md={2} xs={2} sm={2} >
                                         ₹{item.price}    
                                          </Col>
-                                         <Col md={2}>
+                                         <Col md={3} xs={4} sm={3}>
                                          <FormControl as='select' value={item.qty} onChange={(e)=>
                                             dispatch(addToCart(item.product,Number(e.target.value)))}>
                                             {
@@ -59,7 +59,11 @@ const CartScreen = ({match,location,history}) => {
                                         }   
                                         </FormControl>  
                                         </Col>
-                                        
+                                        <Col md={2} xs={1} style={{ padding: '0px' }}> 
+                                            <Button type='button' variant='light' >
+                                                <i className='fas fa-trash' />
+                                            </Button>
+                                        </Col>
                                     </Row>
                                 </ListGroupItem>
                              
@@ -75,12 +79,14 @@ const CartScreen = ({match,location,history}) => {
             </Col>
             <Col md={4}>
                 <Card>
-                    <ListGroup variant='flush'>
+                    <ListGroup variant='flush' >
                         <ListGroupItem className="border-bottom-0" id='subtotal'>
                             <h3>Subtotal   ({cartItems.reduce((acc,item)=> acc+item.qty,0)}) items</h3>
                             ₹{cartItems.reduce((acc,item)=>acc+item.qty*item.price,0).toFixed(2)}
                         </ListGroupItem>
-                  
+                        <ListGroupItem id='checkout'>
+                            <Button type='button' className='btn-block' disabled={cartItems.length===0} >Proceed To Checkout</Button>
+                        </ListGroupItem>
                     </ListGroup>
                 </Card>
 
